e1897e4fca3ea854f1707b84a1a4a3257768a829
Shaikh-Qasim <shaikhqasim563@outlook.com>
2019-02-08 17:39:36 +0500

    Project Created and implemented GetAuthenticationQuestions

---
+++ TransunionApi/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using CC2Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TransunionApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly CC2Client _client;
+
+        public AuthController(CC2Client client)
+        {
+            _client = client;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public GetAuthenticationQuestionsResponse Get()
+        {
+            return _client.GetAuthenticationQuestionsAsync(new GetAuthenticationQuestionsRequest
+            {
+                AccountCode = "123456789",
+                AccountName = "CC2PartnerTesting1",
+                RequestKey = "12345676789089",
+                ClientKey = "ND20111111-testauth",
+                Customer = new Customer
+                {
+                    CurrentAddress = new Address
+                    {
+                        AddressLine1 = "2635 W EVERGREEN AV",
+                        City = "VISALIA",
+                        State = "IL",
+                        Zipcode = "67450"
+                    },
+                    DateOfBirth = new DateTime(1957 - 09 - 01),
+                    FullName = new Name
+                    {
+                        FirstName = "Anne",
+                        LastName = "Test"
+                    },
+                    Ssn = "000000001"
+                },
+                ServiceBundleCode = "CC2PartnerTesting1Authentication"
+            }).GetAwaiter().GetResult();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TransunionApi/Startup.cs
@@ -0,0 +1,52 @@
+﻿using CC2Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.ServiceModel;
+
+namespace TransunionApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<CC2Client>(provider =>
+            {
+                var client = new CC2Client(CC2Client.EndpointConfiguration.BasicHttpsBinding_ICC2);
+
+                client.ClientCredentials.UserName.UserName = Configuration["CC2Service:Username"];
+                client.ClientCredentials.UserName.Password = Configuration["CC2Service:Password"];
+                client.Endpoint.Address = new EndpointAddress(Configuration["CC2Service:BaseUrl"]);
+
+                return client;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
